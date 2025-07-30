0b97ed8ba8c06411c2df8269fb5521fdc49d6382
Mohan Manikanta <mohan.manikanta@hashicorp.com>
2025-07-31 01:40:22 +0530

    Commit 1 Test

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// This is a simple Go program that prints "Hello, World!" to the console.
+// It serves as a basic example of a Go application structure.
+// To run this program, save it in a file named main.go and execute the command:
+// go run main.go
+// The program uses the "fmt" package to handle formatted I/O operations.
+// The main function is the entry point of the program, where execution begins.
+// The fmt.Println function is used to output the string to the console.
+// This code can be extended with more functionality as needed.
+// The program is structured to be easily readable and maintainable.
+// Ensure you have Go installed on your machine to run this code.
+// You can modify the string in fmt.Println to print different messages.
+// This code is a good starting point for learning Go programming.
